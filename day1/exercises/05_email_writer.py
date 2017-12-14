0a88bde825d6498a5d13cb0cd162589951b0afad
pirhoo <hello@pirhoo.com>
2017-12-14 19:13:52 +0100

    Added solution

--- day1/exercises/05_email_writer.py
+++ day1/exercises/05_email_writer.py
@@ -8,13 +8,20 @@ Transformez le nom pour être avec une adresse email et affichez le.
 def emailify(name, domain):
     """
      Ce code doit:
-     1) remplacer les epsaces par des
+     1) remplacer les epsaces par des "."
      2) convertir en minuscule
      3) remplacer les caractères spéciaux (é, è, ë) par des caractères non-accentués
      4) afficher le resutat
      5) retourner le resutat
     """
-    
+    name = name.replace(" ", ".")
+    name = name.lower()
+    name = name.replace('é', 'e')
+    name = name.replace('è', 'e')
+    name = name.replace('ë', 'e')
+
+    print ("%s@%s") % (name, domain)
+
 
 emailify("Mathilde ANDROUËT", "iledefrance.fr")
 emailify("Clémentine AUTAIN", "iledefrance.fr")
