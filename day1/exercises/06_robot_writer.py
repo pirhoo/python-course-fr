58d2e15a7a55f0a705ec019a54250f4be39d22db
pirhoo <hello@pirhoo.com>
2017-12-14 19:11:55 +0100

    Added solution

--- day1/exercises/06_robot_writer.py
+++ day1/exercises/06_robot_writer.py
@@ -7,9 +7,19 @@ Voir 06_robot_writer.md pour les consignes
 total_unemployment_2009 = 8.8
 total_unemployment_2016 = 9.6
 
-def write_story(municipality. unemployment_2009, unemployment_2016):
-    text = "Générer une histoire..."
-    print(text)
+def write_story(municipality, unemployment_2009, unemployment_2016):
+    unemployment_diff = unemployment_2016 - unemployment_2009
+    total_unemployment_diff = total_unemployment_2016 - total_unemployment_2009
+    if unemployment_2016 > unemployment_2009:
+        print("In 2014, unemployment in %s was %s higher than before the financial crisis in 2009." % (municipality, unemployment_diff))
+        print("It increased from %s%% to %s%%." % (unemployment_2009, unemployment_2016))
+    else:
+        print("In 2014, unemployment in %s was %s lower than before the financial crisis in 2009." % (municipality, -unemployment_diff))
+        print("It decreased from %s%% to %s%%." % (unemployment_2009, unemployment_2016))
+    if unemployment_diff > total_unemployment_diff:
+        print("The development of this city has been a little worse than in the country where the unemployment rate during the same period grew by %s" % total_unemployment_diff)
+    else:
+        print("The development of this city has been a little better than in the country where the unemployment rate during the same period grew by %s" % total_unemployment_diff)
 
 """
 Tests du robot!
