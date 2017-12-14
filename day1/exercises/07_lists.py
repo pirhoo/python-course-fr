d96017d7039c265b762805c6a9c889728695279d
pirhoo <hello@pirhoo.com>
2017-12-14 19:17:10 +0100

    Added answer

--- day1/exercises/07_lists.py
+++ day1/exercises/07_lists.py
@@ -11,4 +11,5 @@ politicians = ["Barack Obama", "Hillary Clinton", "Joe Biden",
 parties = ["Democratic Party", "Republican Party", "Libertarian Party",
            "Green Party", "Constitution Party"]
 
-print("After a tense race, %s from the %s is elected as President of the USA." % (XX, XX) )
+print("After a tense race, %s from the %s is elected as President of the USA." % (politicians[0], parties[0]) )
+print("After a tense race, %s from the %s is elected as President of the USA." % (politicians[4], parties[4]) )
